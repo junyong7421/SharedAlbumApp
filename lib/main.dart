ef2cb115226cb9e5485e90a5488e8b76ce580363
Yoonji-Kang <jessica9756@hansung.ac.kr>
2025-08-03 03:22:19 +0900

    shared_album_list_screen 화면

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'screens/edit_screen.dart'; // 🔹 edit_screen.dart import
 import 'screens/login_choice_screen.dart';
 import 'screens/login_screen.dart';
 import 'screens/signup_screen.dart';
+import 'screens/shared_album_list_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         scaffoldBackgroundColor: Color(0xFFEFEFFF), // 🔹 배경색 적용
       ),
-      home: LoginChoiceScreen(), // LoginChoiceScreen LoginScreen SignUpScreen 🔥 여기서 EditScreen을 첫 화면으로 설정
+      home: SharedAlbumListScreen(), // SharedAlbumListScreen LoginChoiceScreen LoginScreen SignUpScreen 🔥 여기서 EditScreen을 첫 화면으로 설정
     );
   }
 }
