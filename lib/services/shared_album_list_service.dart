2f26379e40cded600ef6cbf45c74acbdd028d415
Yoonji-Kang <jessica9756@hansung.ac.kr>
2025-09-18 19:04:08 +0900

    하트수정중

--- lib/services/shared_album_list_service.dart
+++ lib/services/shared_album_list_service.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+//
 class SharedAlbumListService {
   SharedAlbumListService._();
   static final SharedAlbumListService instance = SharedAlbumListService._();
